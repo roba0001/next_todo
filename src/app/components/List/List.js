59c216aded86cbf265eeb34ce5dbac5c78581583
Ronja <roba0001@stud.kea.dk>
2024-11-05 16:30:26 +0100

    init

---
+++ src/app/components/List/List.js
@@ -0,0 +1,11 @@
+import ListItem from "@/app/components/ListItem/ListItem";
+
+export default function List() {
+  return (
+    <>
+      <ListItem></ListItem>
+      <ListItem></ListItem>
+      <ListItem></ListItem>
+    </>
+  );
+}
---
+++ src/app/components/ListItem/ListItem.js
@@ -0,0 +1,7 @@
+export default function ListItem() {
+  return (
+    <>
+      <p>List Item</p>
+    </>
+  );
+}
